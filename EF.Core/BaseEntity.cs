b0c4fb06ae409dd8c74471570cb3196f2cb8351f
sbhide <surajbhide@gmail.com>
2017-04-08 10:33:42 -0500

    Added Ef core

---
+++ EF.Core/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Core
+{
+    public abstract class BaseEntity
+    {
+        public int ID { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateModified { get; set; }
+        public string IP { get; set; }
+    }
+}
---
+++ EF.Core/Data/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Core.Data
+{
+    public class Book : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateTime DatePublished { get; set; }
+    }
+}
