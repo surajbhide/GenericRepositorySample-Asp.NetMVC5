a47417fde0287741cd8f7145e1e44088ce81fad5
sbhide <surajbhide@gmail.com>
2017-04-08 10:34:22 -0500

    first version of ef.web - the web part. not complete yet.

---
+++ EF.Web/Controllers/BookController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EF.Core.Data;
+using EF.Data;
+
+namespace EF.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private UnitOfWork _unitOfWork = new UnitOfWork();
+        private Repository<Book> bookRepository;
+
+        public BookController()
+        {
+            bookRepository = _unitOfWork.Repository<Book>();
+        }
+
+        public ActionResult Index() => View(bookRepository.Table.ToList());
+
+        public ActionResult CreateEditBook(int? id)
+        {
+            Book model = new Book();
+            if (id.HasValue)
+                model = bookRepository.GetById(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult CreateEditBook(Book model)
+        {
+            if (model.ID == 0)
+            {
+                model.DateModified = DateTime.Now;
+                model.DateAdded = DateTime.Now;
+                model.IP = Request.UserHostAddress;
+                bookRepository.Insert(model);
+            }
+            else
+            {
+                var editModel = bookRepository.GetById(model.ID);
+                editModel.Title = model.Title;
+                editModel.Author = model.Author;
+                editModel.ISBN = model.ISBN;
+                editModel.DatePublished = model.DatePublished;
+                editModel.DateModified = DateTime.Now;
+                editModel.IP = Request.UserHostAddress;
+                bookRepository.Update(editModel);
+            }
+            _unitOfWork.Complete();
+
+            if (model.ID > 0)
+                return RedirectToAction("Index");
+
+            return View(model);
+        }
+
+        public ActionResult DeleteBook(int id)
+        {
+            Book model = bookRepository.GetById(id);
+            return View(model);
+        }
+    }
+}
